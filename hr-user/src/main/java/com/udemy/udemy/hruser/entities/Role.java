3ce9160eead9279a8fee0eb05da0eb31183281d5
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-07-10 21:34:03 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/udemy/udemy/hruser/entities/Role.java
+++ hr-user/src/main/java/com/udemy/udemy/hruser/entities/Role.java
@@ -1,7 +1,6 @@
 package com.udemy.udemy.hruser.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,39 +11,44 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_role")
 public class Role implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String roleName;
-	
+
 	public Role() {
 	}
-	
+
 	public Role(Long id, String roleName) {
 		super();
 		this.id = id;
 		this.roleName = roleName;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getRoleName() {
 		return roleName;
 	}
+
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(roleName);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
 	}
 
 	@Override
@@ -56,9 +60,11 @@ public class Role implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Role other = (Role) obj;
-		return Objects.equals(roleName, other.roleName);
+		if (roleName == null) {
+			if (other.roleName != null)
+				return false;
+		} else if (!roleName.equals(other.roleName))
+			return false;
+		return true;
 	}
-
-
-	
 }
